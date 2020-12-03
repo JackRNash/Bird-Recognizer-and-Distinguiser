b582cfa2eed2a9017182f2fcbf4697952376546a
jrn79 <jrn79@cornell.edu>
2020-12-03 13:28:44 -0800

    Fixed jpeg missing and added OSX support

--- utils/resize_imgs.py
+++ utils/resize_imgs.py
@@ -1,21 +1,24 @@
 from PIL import Image
 import os
 
-path = '..\\dataset\\'
+ON_WINDOWS = True
+slash = '\\' if ON_WINDOWS else '/'
+
+path = '..' + slash + 'dataset' + slash
 bird_dirs = [x for x in os.listdir(path) if os.path.isdir(path+x)]
 print(bird_dirs)
 
 def resize_img():
   for bird in bird_dirs:
     for item in os.listdir(path+bird):
-      file_ = path + bird + '\\' + item
+      file_ = path + bird + slash + item
       if os.path.isfile(file_):
         filename, extension = os.path.splitext(file_)
-        if extension == '.jpg':
+        if extension == '.jpg' or extension == 'jpeg':
           print(filename)
           image = Image.open(file_)
           resized = image.resize((256, 256), Image.ANTIALIAS)
-          resized.save(filename+'.jpg', 'JPEG', quality=90)
+          resized.save(filename+'.' + extension, 'JPEG', quality=90)
         # elif extension == '':
         #   print('REMOVED', filename)
         #   os.remove(filename)
