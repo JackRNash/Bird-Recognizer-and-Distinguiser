f661541035f26a978acb950649febdbedd51b850
aeg249 <aeg249@cornell.edu>
2020-12-06 21:24:25 -0500

    std error

--- models/cnn/train.py
+++ models/cnn/train.py
@@ -1,13 +1,18 @@
 import torch
-import torchvision
-import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
+import torchvision
+import matplotlib.pyplot as plt
+import time
 import os
+import copy
 
 from torchvision import datasets, models, transforms
+from tqdm import tqdm
 
 data_dir = '../../dataset/'
-batch_size = 1
+batch_size = 8
 
 def calc_mean_std(dataloader):
     """
@@ -19,14 +24,16 @@ def calc_mean_std(dataloader):
     mean = []
     std = []
 
+    print("calculating mean\n")
+
     # Find and append the mean and standard of each image in dataloader
     for i, data in enumerate(dataloader, 0):
+        print(i)
         img, label = data
+        print(img)
         numpy_image = img.numpy()
-
         batch_mean = np.mean(numpy_image, axis=(0,2,3))
         batch_std = np.std(numpy_image, axis=(0,2,3))
-
         mean.append(batch_mean)
         std.append(batch_std)
 
@@ -43,11 +50,18 @@ def preprocess_data():
     """
     TO DO
     """
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+
+    print("in preprocess\n")
     # Creates a dataloader object for the training and validation sets
-    train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), None)
+    train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), transform)
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
 
-    mean, std = calc_mean_std(train_dataloader)
+    print("made dataloaders\n")
+
+    mean, std = calc_mean_std(tqdm(train_dataloader))
 
     return transforms.Compose([
         transforms.ToTensor(),
