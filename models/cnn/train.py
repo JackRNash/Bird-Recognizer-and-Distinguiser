0e446b581c27b3cd6d0f9d7be69c7bfd24e98901
aeg249 <aeg249@cornell.edu>
2020-12-08 17:10:59 -0500

    Pytorch normalization code

--- models/cnn/train.py
+++ models/cnn/train.py
@@ -11,12 +11,23 @@ import copy
 from torchvision import datasets, models, transforms
 from tqdm import tqdm
 
+torch.manual_seed(4701)
+np.random.seed(4701)
+
 data_dir = '../../dataset/'
 batch_size = 8
 
+
 def calc_mean_std(dataloader):
     """
-    TO DO
+    Returns a list of the average RGB values and a list of the standard of
+    the RGB values.
+
+    Extracts the mean and standard of RGB values of every image in dataloader
+    and then returns the average.
+
+    Parameter dataloader: images to find the mean and standard of
+    Precondition: dataloader is a Torch dataloader object
 
     CITATION: This function was taken and adapted from the following source:
         https://forums.fast.ai/t/image-normalization-in-pytorch/7534/7
@@ -24,44 +35,41 @@ def calc_mean_std(dataloader):
     mean = []
     std = []
 
-    print("calculating mean\n")
-
     # Find and append the mean and standard of each image in dataloader
-    for i, data in enumerate(dataloader, 0):
-        print(i)
-        img, label = data
-        print(img)
-        numpy_image = img.numpy()
-        batch_mean = np.mean(numpy_image, axis=(0,2,3))
-        batch_std = np.std(numpy_image, axis=(0,2,3))
+    for data in dataloader:
+        img, _ = data
+
+        batch_mean = torch.mean(img, (0,2,3))
+        batch_std = torch.std(img, (0,2,3))
+
         mean.append(batch_mean)
         std.append(batch_std)
 
     # Find and return the mean and standard of every image in dataloader
-    mean = np.array(mean).mean(axis=0)
-    std = np.array(std).mean(axis=0)
+    mean = np.mean([m.numpy() for m in mean], axis=0)
+    std = np.mean([s.numpy() for s in std], axis=0)
 
-    print(mean)
-    print(std)
     return mean, std
 
 
 def preprocess_data():
     """
-    TO DO
+    Returns the transformation necessary to normalize the image data.
+
+    Creates a normalization transformation that results in the training data
+    having a mean of 0 and a standard deviation of 1. 
     """
     transform = transforms.Compose([
         transforms.ToTensor()
     ])
 
-    print("in preprocess\n")
     # Creates a dataloader object for the training and validation sets
-    train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), transform)
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), \
+        transform)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, \
+        batch_size=batch_size, shuffle=True, num_workers=4)
 
-    print("made dataloaders\n")
-
-    mean, std = calc_mean_std(tqdm(train_dataloader))
+    mean, std = calc_mean_std(train_dataloader)
 
     return transforms.Compose([
         transforms.ToTensor(),
