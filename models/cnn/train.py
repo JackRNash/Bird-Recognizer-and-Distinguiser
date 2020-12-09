1aa1a476a8853788c2945669e00ce6e9f428ae11
aeg249 <aeg249@cornell.edu>
2020-12-09 16:11:43 -0500

    initial cnn nn

--- models/cnn/train.py
+++ models/cnn/train.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 import torchvision
@@ -19,6 +20,29 @@ input_dim = 256    # Our dataset images are 256 x 256
 output_dim = 10
 batch_size = 8
 num_epochs = 5     # investigate making larger
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.pool_compress = nn.MaxPool2d(8,8)
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(5 * 5 * 16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool_compress(x)
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = x.view(-1, 5 * 5 * 16)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
 def calc_mean_std(dataloader):
@@ -90,5 +114,56 @@ if __name__ == '__main__':
         for x in ['train', 'val']}
     # Create training and validation dataloaders
     dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], \
-        batch_size=batch_size, shuffle=True, num_workers=4) \
+        batch_size=batch_size, shuffle=True, num_workers=0) \
         for x in ['train', 'val']}
+
+    net = Net()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    trainloader = dataloaders_dict['train']
+
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 100 == 99:    # print every 100 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 100))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+    PATH = './lenet5.pth'
+    torch.save(net.state_dict(), PATH)
+
+    net = Net()
+    net.load_state_dict(torch.load(PATH))
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in dataloaders_dict['val']:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
